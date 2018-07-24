7f1d6d5561952de9f93f0b94e775a05fe266fa5f
rain1 <rain1@airmail.cc>
2018-07-24 21:44:17 +0100

    Update parser.rkt
    allow whitespace after quote markers

--- parser.rkt
+++ parser.rkt
@@ -4,6 +4,7 @@ _ < [ \t\n]*;
 OB < '(' ;
 CB < ')' ;
 DQ < ["] ;
+
 s-exp <- atom / list / quote / quasiquote / unquote ;
 atom <- boolean / symbol / number / string ;
 list <-- OB _ (s-exp _)*  CB ;
@@ -12,11 +13,10 @@ symbol <-- [a-zA-Z\-]+ ;
 number <-- [0-9]+ ;
 string <-- DQ [^"]* DQ ;
 
-
 SQ < '\'' ;
 BQ < '`' ;
 COMMA < ',' ;
 
-quote <-- SQ s-exp ;
-quasiquote <-- BQ s-exp ;
-unquote <-- COMMA s-exp ;
+quote <-- SQ _ s-exp ;
+quasiquote <-- BQ _ s-exp ;
+unquote <-- COMMA _ s-exp ;
