45ae779144047f36fe466f6767b5d1bcaf5e7b11
rain1 <rain1@airmail.cc>
2018-07-25 19:10:12 +0100

    add tests for the change

--- test.rkt
+++ test.rkt
@@ -30,3 +30,8 @@
 (check-equal?
  (s-exp->scheme (peg s-exp "`(a b ,(c d e))"))
  '(quasiquote (a b (unquote (c d e)))))
+
+;; + and numbers
+(check-equal?
+ (s-exp->scheme (peg s-exp "(+ 1 2)"))
+ '(+ 1 2))
